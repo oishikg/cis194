ae81321f470bdf05671e04d3360a81753e6a1d2c
OIshik Ganguly <oishik.ganguly@u.yale-nus.edu.sg>
2019-05-18 18:29:13 +0800

    starting on hw6

---
+++ week6/hw6/Fibonacci.hs
@@ -0,0 +1,44 @@
+module Fibonacci where
+
+
+{- Fibonacci numbers:
+
+The Fibonacci numbers Fn are defined as the sequence of integers,
+beginning with 0 and 1, where every integer in the sequence is the
+sum of the previous two. That is,
+
+F0 = 0
+F1 = 1
+Fn = Fn 1 + Fn 2 (n   2)
+
+For example, the first fifteen Fibonacci numbers are
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, . . .
+
+It s quite likely that you ve heard of the Fibonacci numbers before.
+The reason they re so famous probably has something to do with the
+simplicity of their definition combined with the astounding variety of
+ways that they show up in various areas of mathematics as well as art
+and nature. -}
+
+
+{- Exercise 1
+
+Translate the above definition of Fibonacci numbers directly into a
+recursive function definition of type
+fib :: Integer -> Integer
+so that fib n computes the nth Fibonacci number Fn.
+Now use fib to define the infinite list of all Fibonacci numbers,
+fibs1 :: [Integer] -}
+
+
+
+
+
+
+
+
+
+
+
+
